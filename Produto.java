00b7db2d2545586e168c685bf884eb9bf5d09fea
danimagalhaes9 <danielmagalhaes20177@icloud.com>
2026-04-30 08:25:18 -0300

    Add files via upload

---
+++ Produto.java
@@ -0,0 +1,12 @@
+package nextfood;
+
+public class Produto {
+    String nome;
+    double preco;
+
+    //construir produto
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+}
