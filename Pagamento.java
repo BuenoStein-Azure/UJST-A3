32cc9ff43baa474ac6d8e95f3e9b7bc053393c37
danimagalhaes9 <danielmagalhaes20177@icloud.com>
2026-04-30 08:24:45 -0300

    Add files via upload

---
+++ Pagamento.java
@@ -0,0 +1,32 @@
+package nextfood;
+
+public class Pagamento {
+
+    public double processarPagamento(double total, int metodoPagamento) {
+
+        System.out.println("\nForma de pagamento:");
+
+        double taxa = 0;
+
+        switch (metodoPagamento) {
+            case 1:
+                System.out.println("Pagamento no DÉBITO selecionado.");
+                break;
+
+            case 2:
+                System.out.println("Pagamento no CRÉDITO selecionado.");
+                taxa = 0.10;
+                break;
+
+            case 3:
+                System.out.println("Pagamento via PIX selecionado.");
+                break;
+
+            default:
+                System.out.println("Opção inválida de pagamento.");
+                break;
+        }
+
+        return total + (total * taxa);
+    }
+}
